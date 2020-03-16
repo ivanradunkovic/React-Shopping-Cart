a308ba0e880774a2ab3e0feb3a655c9d1bbb1680
Ivan <ivan1501@gmail.com>
2020-03-16 17:40:02 +0100

    Added dummy product and list. Later will add real data.

---
+++ src/components/productList.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Product from './Product';
+ 
+ 
+class ProductList extends Component
+{
+    render() {
+        return (
+            <div className="container">
+                <h2>Product List</h2>
+                <br/>
+                <div className="row">
+                    <Product />
+                    <Product />
+                    <Product />
+                </div>
+            </div>
+        )
+    }
+}
+ 
+ 
+export default ProductList;
