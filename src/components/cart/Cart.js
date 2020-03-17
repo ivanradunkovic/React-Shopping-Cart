7aeede58e149bd80ab1f628a29ff6d0114969d4b
Ivan <ivan1501@gmail.com>
2020-03-17 11:29:07 +0100

    Items and quantity in cart are now shown properly and accurate. Now I can add more items in cart and update and delete.

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,9 +1,44 @@
 import React, { Component } from 'react';
 import Item from './Item';
+import { connect } from 'react-redux';
  
 class Cart extends Component
 {
     render() {
+ 
+        let total = 0;
+ 
+        this.props.cart.map(item => total += item.product.price * item.quantity);
+ 
+        const cart  = this.props.cart.length > 0?(
+            <div>
+                <div className="panel-body">
+                    {
+                        this.props.cart.map(item => {
+                            return (
+                                <div key={item.product.id}>
+                                    <Item item={item} />
+                                    <hr />
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+                <div className="panel-footer">
+                    <div className="row text-center">
+                        <div className="col-xs-11">
+                            <h4 className="text-right">Total <strong>{total.toFixed(2)} Kn</strong></h4> 
+                        </div>
+ 
+                    </div>
+                </div>
+            </div>
+ 
+        ) : (
+            <div className="panel-body">
+                <p>Cart is empty</p>
+            </div>
+        )
  
         return (
                 <div className="container">
@@ -16,20 +51,13 @@ class Cart extends Component
                                             <div className="col-xs-6">
                                                 <h5><span className="glyphicon glyphicon-shopping-cart"></span> My Shopping Cart</h5>
                                             </div>
+ 
                                         </div>
                                     </div>
                                 </div>
-                                <div className="panel-body">
-                                       <Item />
-                                       <Item />
-                                </div>
-                                <div className="panel-footer">
-                                   <div className="row text-center">
-                                      <div className="col-xs-11">
-                                        <h4 className="text-right">Total <strong>Kn</strong></h4>
-                                      </div>
-                                   </div>
-                                 </div>
+ 
+                                { cart }
+ 
                             </div>
                         </div>
                     </div>
@@ -38,4 +66,11 @@ class Cart extends Component
     }
 }
  
-export default Cart;
+const mapStateToProps = (state) => {
+ 
+  return {
+      cart: state.cart.cart
+  }
+};
+ 
+export default connect(mapStateToProps)(Cart);
